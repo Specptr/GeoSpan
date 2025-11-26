c596044f9328f569068e753b08d7b1d3fa6678a9
EnoLaice <152045548+Specptr@users.noreply.github.com>
2025-11-26 18:41:59 +0000

    v1.3 Update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 # GeoSpan by EnoLaice
-# 11/6/2025 - 11/8/2025
-# v1.0
-# 11/12/2025
-# v1.1
-# 11/13/2025
-# v1.2
+# 11/6/2025 alpha
+# 11/8/2025 v1.0
+# 11/12/2025 v1.1
+# 11/13/2025 v1.2
+# 11/26/2025 v1.3
 
 import sys
 from GS_ui import QApplication, GeoSpanUI
